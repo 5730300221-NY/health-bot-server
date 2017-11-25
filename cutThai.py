b2587895f844cf6d2015511a284d9d0b8d441baa
unknown <salapao_oo@hotmail.com>
2017-11-25 20:46:51 +0700

    edit deepcut

--- cutThai.py
+++ cutThai.py
@@ -1,7 +1,8 @@
 # encoding: utf-8
 import deepcut
 
-def cutThai(list_word):
-    a = deepcut.tokenize(list_word)
-    out = u' '.join(list_word).encode('utf-8').strip()
+def cutThai(text):
+    list_word = deepcut.tokenize(text)
+    out = u'|'.join(list_word).encode('utf-8').strip()
     return out
+
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
+# encoding: utf-8
 from wit import Wit
 from cutThai import cutThai
-from pythainlp.tokenize import word_tokenize
+# from pythainlp.tokenize import word_tokenize
 
 access_token = "WMN4JBK2HPW66APORX65EHLLJBYGEPMF"
 client = Wit(access_token = access_token)
@@ -10,8 +11,8 @@ def wit_response(message_text):
     # token = word_tokenize(message_text)
     # text =""
     # for word in token:
-    #     if(word != " "):
-    #         text += word + " "
+    #      if(word != " "):
+    #          text += word + " "
     text = cutThai(message_text)
     resp = client.message(text)
 
@@ -20,15 +21,12 @@ def wit_response(message_text):
     reply = ""
 
     try:
-       """" entity = list(resp['entities'])
-        value = resp['entities'][entity][0]['value']"""
+       # entity = list(resp['entities'])
+        # value = resp['entities'][entity][0]['value']
        for entity in list(resp['entities']):
            reply += entity + " : " +  resp['entities'][entity][0]['value']
 
     except:
         pass
-
-    #if(entity1 == "age_of_person"): return ("อายุ " + value + "แล้วเหรอ")
-    #elif (entity1 == "type" & entity2 == "amount"): return ("กิน"+ value1 + "มา " + value2 + " จานเหรอ")
 
     return (text,reply)
